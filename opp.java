c37b7b7abd3e5a7b6b4fbbf40d6e27b0594842d1
kibetcollinskenduiwa <kibetcollinskenduiwa@gmail.com>
2021-04-02 09:01:37 +0300

    Java

---
+++ opp.java
@@ -0,0 +1,10 @@
+public class oop{
+    public static void main(String[]args){
+        System.out.println("hello java");
+        User user = new User();
+        user.name="Collins";
+        user.membership="Premium";
+        
+
+    }
+}
